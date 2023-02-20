b5ad740bf33e112e125cdb0c5bb4d87b63f5bbf7
Fire Owl <61467254+Owl-Man@users.noreply.github.com>
2023-02-20 22:20:57 +0300

    Coroutines are stabilized
    Coroutines are stabilized when enabling/disabling an object with LongPressEventTrigger script

--- LongPressEventTrigger.cs
+++ LongPressEventTrigger.cs
@@ -3,34 +3,43 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class LongPressEventTrigger : UIBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+namespace System
 {
-    [Tooltip("How often events should be triggered when the button is pressed (The lower the value, the more often; the higher the value, the less often)")]
-    public float tact = 0.062f;
-
-    [Space(10)]
-
-    public UnityEvent onLongPress = new UnityEvent();
-
-    private bool isPointerDown;
-
-    protected override void Start()
+    public class LongPressEventTrigger : UIBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
     {
-        base.Start();
-        StartCoroutine(EventUpdate());
+        [Tooltip("How often events should be triggered when the button is pressed (The lower the value, the more often; the higher the value, the less often)")]
+        public float tact = 0.062f;
+
+        [Space(10)]
+
+        public UnityEvent onLongPress = new UnityEvent();
+
+        private bool _isPointerDown;
+        
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            StartCoroutine(EventUpdate());
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            StopCoroutine(EventUpdate());
+        }
+
+        private IEnumerator EventUpdate()
+        {
+            if (_isPointerDown) onLongPress.Invoke();
+
+            yield return new WaitForSeconds(tact);
+            StartCoroutine(EventUpdate());
+        }
+
+        public void OnPointerDown(PointerEventData eventData) => _isPointerDown = true;
+
+        public void OnPointerUp(PointerEventData eventData) => _isPointerDown = false;
+
+        public void OnPointerExit(PointerEventData eventData) => _isPointerDown = false;
     }
-
-    private IEnumerator EventUpdate()
-    {
-        if (isPointerDown) onLongPress.Invoke();
-
-        yield return new WaitForSeconds(tact);
-        StartCoroutine(EventUpdate());
-    }
-
-    public void OnPointerDown(PointerEventData eventData) => isPointerDown = true;
-
-    public void OnPointerUp(PointerEventData eventData) => isPointerDown = false;
-
-    public void OnPointerExit(PointerEventData eventData) => isPointerDown = false;
 }
